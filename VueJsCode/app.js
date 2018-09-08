170f90f094c58b3dbd3c22331cc8396b4eb553b6
alkeshcharpe <42964517+alkeshcharpe@users.noreply.github.com>
2018-09-08 23:18:31 +0530

    OK

--- VueJsCode/app.js
+++ VueJsCode/app.js
@@ -2,5 +2,7 @@ var myVue = new Vue({
     el:"#our-app",
     data:{
       message: "This is our first code"
-    }
+    },
+    ulink:"https://www.youtube.com/watch?v=BqCaoGuRDC4&index=4&list=PLT9miexWCpPUZ-xD9s70PhhP37blPSuax"
+
   });
---
+++ VueJsCode/v-bind example.js
@@ -0,0 +1,7 @@
+var TestUrl = new Vue({
+  el:"#TestUrl",
+  data:{
+    name:"Youtube Link",
+    ulink:"https://www.youtube.com/watch?v=BqCaoGuRDC4&index=4&list=PLT9miexWCpPUZ-xD9s70PhhP37blPSuax"
+  }
+});
