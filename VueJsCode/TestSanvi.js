eaca9886e93e7eb3378847630309b9d4f044c82e
alkeshcharpe <42964517+alkeshcharpe@users.noreply.github.com>
2018-09-14 22:04:40 +0530

    Done

---
+++ VueJsCode/TestSanvi.js
@@ -0,0 +1,13 @@
+var testVue1 = new Vue({
+    el:"#test-vue",
+    data:{
+      message: "This is our Sanvi",
+      ulink: "https://www.google.co.in/"
+    },
+
+    methods:{
+      test_method: function(b){
+        return a =b+10;
+      }
+    }
+  });
